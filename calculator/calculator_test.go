04da339b4f44d577c2d0eb52d91091588c6b6e13
Jesús Jiménez Ballano <jjballano@gmail.com>
2015-02-26 07:31:31 +0000

    Using structs to test

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -3,8 +3,22 @@ package calculator
 import "testing"
 
 func TestSumsTwoNumbers(t *testing.T) {
-	sumValue := Sum(3,5)
-	if sumValue != 8 {
-		t.Errorf("The result of %d + %d must be %d",3,5,8) 
+	cases := []struct {
+		a, b, expected int
+	}{
+		{2,3,5},
+		{0,0,0},
+		{0,1,1},
+		{3,0,3},		
+		{4,-2,2},
+		{-1,-3,-4},
+		{-1,4,3},		
 	}
+	for _,numbers := range cases {
+		sum := Sum(numbers.a, numbers.b)
+		if (sum != numbers.expected){
+			t.Errorf("The result of %d + %d must be %d, but was %d",numbers.a,numbers.b,numbers.expected, sum) 	
+		}
+	}
+
 }
